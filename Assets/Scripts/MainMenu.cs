af0e0ffd155a7e0ce2b6311402d65b46194985a4
Gaetan depin <39808631+gandous@users.noreply.github.com>
2022-12-12 15:59:23 +0100

    improve menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Mirror;
 
-public class Menu : MonoBehaviour
+public class MainMenu : MonoBehaviour
 {
 
     [SerializeField] private TMP_InputField ipInput;
     [SerializeField] private TMP_InputField nameInput;
+    [SerializeField] private NetworkManager manager;
 
     private Color color;
     private GameObject colorPreview;
@@ -47,13 +49,19 @@ public class Menu : MonoBehaviour
     public void OnHostGameClicked()
     {
         Debug.Log("Host game");
+        manager.StartHost(); // Client + Server
+        // manager.StartServer(); Server only
     }
 
     public void OnJoinGameClicked()
     {
-        if (string.IsNullOrEmpty(ipInput.text))
+        if (string.IsNullOrEmpty(ipInput.text)) {
+            Debug.Log("Empty ip address");
             return;
+        }
         Debug.Log("Join game");
         Debug.Log(ipInput.text);
+        manager.networkAddress = ipInput.text;
+        manager.StartClient();
     }
 }
