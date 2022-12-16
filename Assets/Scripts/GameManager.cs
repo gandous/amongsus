611bd7c0791cb3e4e10f07969424f753f5ce7ba5
nathangranet <52495555+nathangranet@users.noreply.github.com>
2022-12-16 16:54:22 +0100

    correcting main menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,13 +10,19 @@ public class GameManager : MonoBehaviour
     {
         Instance = this;
     }
+
+    private List<player_movement> _players = new List<player_movement>();
+
     IEnumerator Start()
     {
-        while (NetworkServer.active != true)
+        while (NetworkServer.active == false || _players.Count != NetworkServer.connections.Count || !_players.TrueForAll(x => x.connectionToClient.isReady))
             yield return null;
-
     }
 
+  /*  public void AddPlayer(player_movement player)
+    {
+        _pl
+    }*/
     // Update is called once per frame
     void Update()
     {
