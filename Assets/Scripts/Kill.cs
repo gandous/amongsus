f27b77ad8363e08afc15454befd7a1b1ace47d2c
Gaetan depin <39808631+gandous@users.noreply.github.com>
2022-12-20 18:23:46 +0100

    add kill

--- Assets/Scripts/Kill.cs
+++ Assets/Scripts/Kill.cs
@@ -21,16 +21,20 @@ public class Kill : NetworkBehaviour
     [Command]
     public void makeKill(string playerName)
     {
-        Debug.Log("kill");
         int i = 0;
         for (; i < GameManager.Instance.Players.Count && GameManager.Instance.Players[i].playerName != playerName; i++);
         player_movement target_player = GameManager.Instance.Players[i];
+        if (target_player.dead == true)
+            return;
+        Debug.Log("kill");
         Transform target_transform = target_player.GetComponent<Transform>();
         Vector3 pos = target_transform.position;
         pos.y -= 0.5f;
-        GameObject body = NetworkManager.Instantiate(deadBody, pos, new Quaternion());
-        NetworkManager.Destroy(target_player.GetComponent<MeshFilter>());
-        NetworkManager.Destroy(target_player.GetComponent<MeshRenderer>());
+        GameObject body = Instantiate(deadBody, pos, new Quaternion());
+        //NetworkManager.Destroy(target_player.GetComponent<MeshFilter>());
+        //NetworkManager.Destroy(target_player.GetComponent<MeshRenderer>());
+        NetworkServer.Spawn(body);
         target_player.Dead();
+        target_player.dead = true;
     }
 }
